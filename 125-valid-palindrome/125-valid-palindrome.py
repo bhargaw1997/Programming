2e13238f9b6a07fefc0d106e607f8301935bd1c7
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2022-03-17 11:18:06 -0500

    Time: 72 ms (45.50%), Space: 14.5 MB (67.89%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,9 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        palin=""
-        lower_s = s.lower()
-        for ch in lower_s:
-            if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z') or (ch>='0' and ch<='9')):
-                palin += ch
-        # print(palin,palin[-1::-1])
-        return True if palin == palin[-1::-1] else False
+
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+
+            i += 1
+            j -= 1
+
+        return True
