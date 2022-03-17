f5740a0de27b8b717f3b9d26f081b853f398c1f5
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2022-03-17 12:50:24 -0500

    Time: 450 ms (23.55%), Space: 22 MB (20.70%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,19 +1,29 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res = ""
-        reslen=0
-        ans = []
+        #base case
+        if not s:
+            return 0
+        ans=0
+        #initialize 2d array
+        dp = [[False]*len(s) for _ in range(len(s))]
+        
+        #1 character palindrom
         for i in range(len(s)):
-            #odd number
-            l,r=i,i
-            while(l>=0 and r<len(s) and s[l]==s[r]):
-                ans.append(s[l:r+1])
-                l-=1
-                r+=1
-            #even number
-            l,r=i,i+1
-            while(l>=0 and r<len(s) and s[l]==s[r]):
-                ans.append(s[l:r+1])
-                l-=1
-                r+=1
-        return len(ans)
+            dp[i][i] = True
+            ans += 1
+        
+        #2 character palindrom
+        for i in range(0,len(s)-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] =True 
+                ans+=1
+        
+        #3 or more characters palindrom
+        for length in range(2,len(s)):
+            i=0
+            for j in range(i+length,len(s)):
+                if dp[i+1][j-1] == True and s[i] == s[j]:
+                    dp[i][j] = True
+                    ans+=1
+                i+=1
+        return ans
