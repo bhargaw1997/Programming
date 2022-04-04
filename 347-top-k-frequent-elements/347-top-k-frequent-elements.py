fdf3cc6cfb8771a1f1f4e47a04309d079c3f0d6d
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2022-04-04 12:57:05 -0500

    Time: 184 ms (21.11%), Space: 18.5 MB (98.63%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,10 +1,4 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dic = collections.Counter(nums)
-        ans = []
-        for i in range(k):
-            max_key = max(dic,key=dic.get)
-            ans.append(max_key)
-            dic[max_key] = 0
-        return ans
+        return [i for i,j in Counter(nums).most_common(k)]
             
