284a224ba50bd00325154b988d98078f20d533af
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2022-04-08 09:39:15 -0500

    Time: 160 ms (38.95%), Space: 18 MB (98.83%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
@@ -17,3 +17,16 @@ class KthLargest:
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
+
+# Given NN as the length of nums and MM as the number of calls to add(),
+
+    # Time complexity: O(N \cdot \log(N) + M \cdot \log(k))O(N⋅log(N)+M⋅log(k))
+
+    # The time complexity is split into two parts. First, the constructor needs to turn nums into a heap of size k. In Python, heapq.heapify() can turn nums into a heap in O(N)O(N) time. Then, we need to remove from the heap until there are only k elements in it, which means removing N - k elements. Since k can be, say 1, in terms of big OO this is N operations, with each operation costing \log(N)log(N). Therefore, the constructor costs O(N + N \cdot \log(N)) = O(N \cdot \log(N))O(N+N⋅log(N))=O(N⋅log(N)).
+
+    # Next, every call to add() involves adding an element to heap and potentially removing an element from heap. Since our heap is of size k, every call to add() at worst costs O(2 * \log(k)) = O(\log(k))O(2∗log(k))=O(log(k)). That means M calls to add() costs O(M \cdot \log(k))O(M⋅log(k)).
+
+# Space complexity: O(N)O(N)
+
+    # The only extra space we use is the heap. While during add() calls we limit the size of the heap to k, in the constructor we start by converting nums into a heap, which means the heap will initially be of size N.
