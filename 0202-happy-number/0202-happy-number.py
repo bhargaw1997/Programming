3bfb1490a29212c93d47062a34590690da9ea26c
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2024-11-09 13:45:29 -0600

    Time: 0 ms (100%), Space: 16.6 MB (55.24%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -8,9 +8,8 @@ class Solution:
                 total_sum += digit**2
             return total_sum
         
-        slow = n
-        fast = get_next(n)
-        while fast != 1 and slow != fast:
-            slow = get_next(slow)
-            fast = get_next(get_next(fast))
-        return fast == 1
+        seen =set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+        return n == 1
