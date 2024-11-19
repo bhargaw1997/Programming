23f6b3a4e49ca0b5fb482d7be5e61e07d3b12b33
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2024-11-18 22:11:35 -0600

    Time: 38 ms (54.27%), Space: 17.3 MB (90.21%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,31 +8,22 @@ class Node:
 """
 
 class Solution:
-    def __init__(self):
-        # Dictionary which holds old nodes as keys and new nodes as its values.
-        self.visitedHash = {}
-
     def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
 
-        if head == None:
+        if not head:
             return None
+        
+        new_list = {}
 
-        # If we have already processed the current node, then we simply return the cloned version of it.
-        if head in self.visitedHash:
-            return self.visitedHash[head]
-
-        # create a new node
-        # with the value same as old node.
-        node = Node(head.val, None, None)
-
-        # Save this value in the hash map. This is needed since there might be
-        # loops during traversal due to randomness of random pointers and this would help us avoid them.
-        self.visitedHash[head] = node
-
-        # Recursively copy the remaining linked list starting once from the next pointer and then from the random pointer.
-        # Thus we have two independent recursive calls.
-        # Finally we update the next and random pointers for the new node created.
-        node.next = self.copyRandomList(head.next)
-        node.random = self.copyRandomList(head.random)
-
-        return node
+        current = head
+        while current:
+            new_list[current] = Node(current.val, None, None)
+            current = current.next
+        
+        current = head
+        while current:
+            new_list[current].next = new_list.get(current.next)
+            new_list[current].random = new_list.get(current.random)
+            current = current.next
+        
+        return new_list[head]
