616c98065ab759ad5b52cc56f5743045a608fb76
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2024-12-20 12:53:34 -0600

    Time: 8 ms (18.93%), Space: 17.8 MB (7.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -12,13 +12,15 @@ class Solution:
 
         while l1 != None or l2 != None or carry != 0:
             l1Val = l1.val if l1 else 0
-            l2Val = l2.val if l2 else 0 
+            l2Val = l2.val if l2 else 0
             columnSum = l1Val + l2Val + carry
             carry = columnSum // 10
-            newVal = columnSum % 10
-            newNode = ListNode(newVal)
+            newNum = columnSum % 10
+            newNode = ListNode(newNum)
             current.next = newNode
             current = newNode
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return dummy.next
+
+    
