3be4467d039cfd6506c1e7805c22040851eb6b8f
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2024-12-20 15:30:23 -0600

    Time: 0 ms (100%), Space: 17.9 MB (6.11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,31 +1,28 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+
         m, n = len(nums1), len(nums2)
-        p1, p2 = 0, 0
+        left, right = 0, m
 
-        # Get the smaller value between nums1[p1] and nums2[p2].
-        def get_min():
-            nonlocal p1, p2
-            if p1 < m and p2 < n:
-                if nums1[p1] < nums2[p2]:
-                    ans = nums1[p1]
-                    p1 += 1
+        while left <= right:
+            partitionA = (left+right) // 2
+            partitionB = (m+n+1) // 2 - partitionA
+
+            maxLeftA = (float("-inf") if partitionA == 0 else nums1[partitionA - 1])
+            minRightA = float("inf") if partitionA == m else nums1[partitionA]
+            maxLeftB = (float("-inf") if partitionB == 0 else nums2[partitionB - 1])
+            minRightB = float("inf") if partitionB == n else nums2[partitionB]
+
+            if maxLeftA <= minRightB and maxLeftB <= minRightA:
+                if (m + n) % 2 == 0:
+                    return (
+                        max(maxLeftA, maxLeftB) + min(minRightA, minRightB)
+                    ) / 2
                 else:
-                    ans = nums2[p2]
-                    p2 += 1
-            elif p2 == n:
-                ans = nums1[p1]
-                p1 += 1
+                    return max(maxLeftA, maxLeftB)
+            elif maxLeftA > minRightB:
+                right = partitionA - 1
             else:
-                ans = nums2[p2]
-                p2 += 1
-            return ans
-
-        if (m + n) % 2 == 0:
-            for _ in range((m + n) // 2 - 1):
-                _ = get_min()
-            return (get_min() + get_min()) / 2
-        else:
-            for _ in range((m + n) // 2):
-                _ = get_min()
-            return get_min()
+                left = partitionA + 1
