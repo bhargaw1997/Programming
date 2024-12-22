2c056d3bf3b95a0070a6109bced68bb782dd8158
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2024-12-21 19:28:04 -0600

    Time: 53 ms (75.04%), Space: 28.8 MB (14.16%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -4,8 +4,8 @@ class Solution:
 
         for i in range(k):
             heappush(min_heap, nums[i])
-
-        for i in range(k,len(nums)):
+        
+        for i in range(k, len(nums)):
             if nums[i] > min_heap[0]:
                 heappop(min_heap)
                 heappush(min_heap, nums[i])
