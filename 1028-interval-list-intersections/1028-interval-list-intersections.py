1b6a3f92ef3a9b4fe20ed003212e1cb562476427
bhargaw1997 <65907436+bhargaw1997@users.noreply.github.com>
2024-12-22 15:18:13 -0600

    Time: 3 ms (91.64%), Space: 18.5 MB (6.25%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -3,18 +3,18 @@ class Solution:
         if (len(firstList) or len(secondList)) < 0:
             return []
 
-        i = 0
-        j = 0
+        i, j = 0, 0
         ans = []
+
         while i < len(firstList) and j < len(secondList):
             lo = max(firstList[i][0], secondList[j][0])
             hi = min(firstList[i][1], secondList[j][1])
 
             if lo <= hi:
-                ans.append([lo,hi])
-            
+                ans.append((lo, hi))
+
             if firstList[i][1] < secondList[j][1]:
                 i += 1
             else:
-                j += 1
-        return ans
+                j +=1
+        return ans  
